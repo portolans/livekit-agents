59cffd2dc687588b387a5e03fe110540732d7e66
Evan Goldschmidt <evan@portola.ai>
2024-10-16 23:03:33 -0700

    chat context: use id instead of inference_id, add timestamp to each message

--- livekit-agents/livekit/agents/utils/__init__.py
+++ livekit-agents/livekit/agents/utils/__init__.py
@@ -4,7 +4,7 @@ from . import aio, audio, codecs, http_context, images
 from .audio import AudioBuffer, combine_frames, merge_frames
 from .exp_filter import ExpFilter
 from .log import log_exceptions
-from .misc import shortuuid, time_ms
+from .misc import message_id, shortuuid, time_ms
 from .moving_average import MovingAverage
 
 EventEmitter = rtc.EventEmitter
@@ -24,4 +24,5 @@ __all__ = [
     "images",
     "audio",
     "aio",
+    "message_id",
 ]
